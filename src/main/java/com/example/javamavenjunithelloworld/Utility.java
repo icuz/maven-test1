38d9d1ea2190cc5dab6b6851998e3442b49fc034
icuz <101340390+icuz@users.noreply.github.com>
2024-09-16 04:25:26 +0000

    VighneshM6969 - added a oddEvenCheck function isEven() to Utility.java

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,13 @@
+package com.example.javamavenjunithelloworld;
+
+public class Vighnesh {
+    public boolean isEven(int n){
+        if (n%2==0){
+            System.out.println("Even");
+            return true;
+        }else{
+            System.out.println("Odd");
+            return false;
+        }
+    }
+}
